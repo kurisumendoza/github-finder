661506b3e15876cebab2e72f105eb85d008f546c
Kristopher Mendoza <creative.kristopher@gmail.com>
2025-12-25 23:35:00 +0800

    feat: add checkIfFollowingUser fetch functionality

--- src/api/github.ts
+++ src/api/github.ts
@@ -19,3 +19,22 @@ export const searchGithubUser = async (query: string) => {
   const data = await res.json();
   return data.items;
 };
+
+export const checkIfFollowingUser = async (username: string) => {
+  const res = await fetch(
+    `${import.meta.env.VITE_GITHUB_API_URL}/users/following/${username}`,
+    {
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_GITHUB_API_TOKEN}`,
+        Accept: 'application/vnd.github+json',
+      },
+    }
+  );
+
+  if (res.status === 204) return true;
+  else if (res.status === 404) return false;
+  else {
+    const errorData = await res.json().catch(() => null);
+    throw new Error(errorData.message || 'Failed to check follow status');
+  }
+};
